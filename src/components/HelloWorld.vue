97e3544e5a58dc6b7e4d5409e149c9512b384f08
Josia RAZAFINJATOVO <rzjosia@gmail.com>
2021-12-08 10:21:43 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+      <v-main>
+        <v-container>
+          <v-row>
+            <v-col v-for="n in 24" :key="n" cols="4">
+              <v-card height="200"></v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
