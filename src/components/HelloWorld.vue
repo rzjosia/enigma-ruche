aba15dc0516323570386858d729b7a0fe3f55783
Adrien GRAMPONE <atale1608@hotmail.fr>
2021-12-08 12:38:35 +0100

    connexion mqtt

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,9 @@
 <template>
+  <div class="hello">
+     <button v-on:click="createConnection">Click</button>
+     <br/>
+     <button v-on:click="doSubscribe">subscribe</button>
+  </div>
   <v-card
     class="mx-auto"
     max-width="344"
@@ -21,15 +26,83 @@
     </v-card-title>
 
   </v-card>
+
 </template>
 
 <script>
+import mqtt from 'mqtt';
 export default {
   name: "HelloWorld",
-  data: function() {
+   data() {
     return {
-      logo: require('../assets/weather.jpg')
+      logo: require('../assets/weather.jpg'),
+      connection: {
+        host: 'broker.hivemq.com',
+        port: 8000,
+        endpoint: '/mqtt',
+        clean: true, // Reserved session
+        connectTimeout: 4000, // Time out
+        reconnectPeriod: 4000, // Reconnection interval
+        // Certification Information
+        clientId: 'clientId-j0Nks3DYA0',
+        username: '',
+        password: '',
+      },
+      subscription: {
+        topic: '/hive/j-118/from_device',
+        qos: 0,
+      },
+      publish: {
+        topic: 'topic/browser',
+        qos: 0,
+        payload: '{ "msg": "Hello, I am browser." }',
+      },
+      receiveNews: '',
+      qosList: [
+        { label: 0, value: 0 },
+        { label: 1, value: 1 },
+        { label: 2, value: 2 },
+      ],
+      client: {
+        connected: false,
+      },
+      subscribeSuccess: false,
     }
+  },
+
+  methods: {
+    // Create connection
+    createConnection() {
+      const { host, port, endpoint, ...options } = this.connection
+      const connectUrl = `ws://${host}:${port}${endpoint}`
+      try {
+        this.client = mqtt.connect(connectUrl, options)
+      } catch (error) {
+        console.log('mqtt.connect error', error)
+      }
+      this.client.on('connect', () => {
+        console.log('Connection succeeded!')
+      })
+      this.client.on('error', error => {
+        console.log('Connection failed', error)
+      })
+      this.client.on('message', (topic, message) => {
+        this.receiveNews = this.receiveNews.concat(message)
+        console.log(`Received message ${message} from topic ${topic}`)
+      })
+    },
+    doSubscribe() {
+      const { topic, qos } = this.subscription
+      this.client.subscribe(topic, { qos }, (error, res) => {
+      if (error) {
+        console.log('Subscribe to topics error', error)
+        return
+      }
+      this.subscribeSuccess = true
+      console.log(res)
+      
+    })
+  },
   }
 };
 </script>
